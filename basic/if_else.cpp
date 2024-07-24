325fe7bb9c4ccedaefa87346d5fe69cda09c5b00
mainak58 <iammainak58@gmail.com>
2024-07-25 03:25:13 +0530

    if else in basic

---
+++ basic/if_else.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int c ;
+    cin>>c ;
+
+    if(c%2==0){
+        cout<<"the number is even" ;
+    }
+    else{
+        cout<<"the number is odd"<<endl ;
+    }
+    return 0;
+}
